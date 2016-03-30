213089ae42aec5288fbd8d131c1a192a5c1339de
Cory Franks and Jake Richmond <jake@>
2016-03-30 09:33:01 -0700

    result if else

--- js/script.js
+++ js/script.js
@@ -9,13 +9,20 @@ var leapYear = function(year) {
   }
 };
 
-
-
-
 $("form#leap-year").submit(function(event){
     event.preventdefault();
     var year = parsInt($("input#year").val());
     var result = leapYear (year);
     $("#result"). text(result);
+
+    $("year").text(year);
+
+    if (!result) {
+      $(".not"). text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("result").show();
   });
 });
