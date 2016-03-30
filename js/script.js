3603cee39c93010b29c3c10e1673f9d842162335
Cory Franks and Jake Richmond <jake@>
2016-03-30 09:15:23 -0700

    more business logic and more logic

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
 
 var leapYear = function(year) {
-  if (year % 100 === 0){
+  if ((year % 100 === 0) && (year % 100 !=== 0)) {
     return true;
   }
-else if (year % 4 === 0) {
-  return true;
-}
   else {
     return false;
   }
