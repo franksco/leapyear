385be7fb18a221c965a277b25187e9a46ca9b6d2
Cory Franks and Jake Richmond <jake@>
2016-03-30 10:03:16 -0700

    boom working

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
 
+// business logic
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !=== 0)) || (year % 400 === 0 )) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0 )) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 };
 
+// user interface logic
 $("form#leap-year").submit(function(event){
-    event.preventdefault();
-    var year = parsInt($("input#year").val());
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
     var result = leapYear (year);
     $("#result"). text(result);
 
     $("year").text(year);
 
-    if (!result) {
-      $(".not"). text("not");
-    } else {
+    if (isNaN(year)) {
+      alert("please put in a year! ex. 1928");
+    } else if (!result) {
+      $(".not").text("not");
+    } else if (result) {
       $(".not").text("");
+    } else {
+      alert("please put in a year! ex. 1928");
     }
 
     $("result").show();
